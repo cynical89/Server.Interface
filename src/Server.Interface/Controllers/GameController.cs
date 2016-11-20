e6e0f841aa3df138a96914c5a5a60cc53ac2a32d
Anthony Michelizzi <cynical89@gmail.com>
2016-11-20 00:25:48 -0800

    Added initial code
    -Started work on game api model and controller

---
+++ src/Server.Interface/Controllers/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Server.Interface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Interface.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameController
+    {
+        /**
+        * POST api/game
+        * 
+        * Starts a new game
+        */
+        [HttpPost]
+        public Game Post([FromBody]string value)
+        {
+            Array[] PlayerArray = new Array[12];
+            Game newGame = new Game()
+            {
+                Id = Guid.NewGuid(), Active = false, TimeAndDate = DateTime.Now, Map = "default", Players = PlayerArray
+            };
+            return newGame;
+        }
+
+        /**
+        * GET api/game
+        * 
+        * Retrieves all games
+        */
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "game1", "game2" };
+        }
+
+        /**
+        * GET api/game/:id
+        * 
+        * Retrieves game by id
+        */
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "game";
+        }
+    }
+}
---
+++ src/Server.Interface/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Interface.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public DateTime TimeAndDate { get; set; }
+        public string Map { get; set; }
+        public Array[] Players { get; set; }
+    }
+}
