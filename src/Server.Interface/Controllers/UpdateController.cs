6ec446b2dd62b094a1d21e1e9497d699ded71a41
Anthony Michelizzi <cynical89@gmail.com>
2016-11-20 00:36:47 -0800

    Added client update controller
    - Also adjusted launch settings for update controllers

---
+++ src/Server.Interface/Controllers/UpdateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Interface.Controllers
+{
+    [Route("api/[controller]")]
+    public class UpdateController
+    {
+        /**
+        * GET api/update/:id
+        * 
+        * Retrieves update status by id
+        */
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id < 2)
+            {
+                return "Client needs to be updated";
+            }
+            else if (id > 2)
+            {
+                return "Client shows an incorrect version number";
+            }
+
+            return "Client is fully updated";
+        }
+    }
+}
