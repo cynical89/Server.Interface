9f11141aa9364bdb2fb534eff742422eda848741
Anthony Michelizzi <cynical89@gmail.com>
2016-11-22 16:54:01 -0800

    update ip

--- src/Server.Interface/Startup.cs
+++ src/Server.Interface/Startup.cs
@@ -39,7 +39,7 @@ namespace Server.Interface
 
             app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
             {
-                Authority = "http://::3000",
+                Authority = "http://104.251.219.52:3000",
                 ScopeName = "api1",
 
                 RequireHttpsMetadata = false
